d1fc2502b9bb567f46fafc9fb956fa9c99659a73
zimeng303 <li_shengnan0303@163.com>
2021-01-25 14:42:25 +0800

    blog

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a0ad548a299a546cc4c0ba4111cda557'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '32be4556-6837-4256-aa1b-baca55d54091'
+};
