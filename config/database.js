f26076e5e8f9a32401090d3e6ca55c2c650e85bd
zimeng303 <li_shengnan0303@163.com>
2021-01-27 11:46:34 +0800

    database

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'blog'),
-        username: env('DATABASE_USERNAME', 'alison'),
+        username: env('DATABASE_USERNAME', 'root'),
         password: env('DATABASE_PASSWORD', '12345678'),
       },
       options: {},
